7bf4e3cb910d3d411c8ae72c2885206a99279e61
socialuni <socialuni@163.com>
2024-09-13 18:16:35 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 // https://vitejs.dev/config/
 import {defineConfig} from "vite";
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
     esbuild: {
         sourcemap: false,
-    }
+    },
+    plugins: [
+        Inspect(),
+    ]
 })
